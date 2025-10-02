8833ddfecf81b24b02d45e9fbb0db6bf957835c3
Abhinav Bhatnagar <abhi.bhatnagar61@yahoo.com>
2025-10-02 17:17:47 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD", "root"),
-        'HOST': os.getenv("DB_HOST", "localhost"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT", "3306"),
     }
 }
