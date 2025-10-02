e81a7773c0c8bcccb31c4d723339e635e28b4281
Abhinav Bhatnagar <abhi.bhatnagar61@yahoo.com>
2025-10-02 13:30:20 +0530

    Set default DB_PASSWORD to 'root' in settings

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "root"),
         'HOST': os.getenv("DB_HOST", "localhost"),
         'PORT': os.getenv("DB_PORT", "3306"),
     }
